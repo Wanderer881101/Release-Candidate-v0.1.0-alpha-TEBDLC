77607183492ffa5b9f6610be486d955d6dddda79
Jonathan Therrien <jonathantherrien2021@gmail.com>
2026-08-22 00:01:32 -0400

    governance: add required attribution to record-validator tests

--- distribution/test_validate_records.py
+++ distribution/test_validate_records.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Jonathan Therrien, Marieville, Québec.
 import copy
 import unittest
 from validate_records import ValidationError, validate_acceptance, validate_event, validate_manifest
